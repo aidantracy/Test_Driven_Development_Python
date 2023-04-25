f51c43a9f227977450b601984a47a86f3fa1d2a0
trac0082 <aidantracy@outlook.com>
2023-04-24 22:20:52 -0600

    Set up templates #1

--- TDD/TDD_app/views.py
+++ TDD/TDD_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return render(request, 'index.html')
 
