a920b80a2a0d6890a1e7ee0a59618419398ae370
trac0082 <aidantracy@outlook.com>
2023-04-27 19:36:28 -0600

    Testing data passing back and forth #1

--- TDD/TDD_app/views.py
+++ TDD/TDD_app/views.py
@@ -9,3 +9,17 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+def some_functions(request):
+    if request.method == "GET":
+        print("a GET request is being made to this route")
+        return render(request, "some_template.html")
+    if request.method == "POST":
+        print("a POST request is being made to this route")
+        return redirect("/")
+
+def some_function(request):
+    if request.method == "POST":
+        val_from_field_one = request.POST["one"]
+        val_from_field_two = request.POST["two"]
+        print(val_from_field_one, val_from_field_two)
+        return redirect('/')
